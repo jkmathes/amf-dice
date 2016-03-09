d027d99edad4ccda08215fa6e2bc5efe114374fb
Howard Tsoi <howard.p.tsoi@intel.com>
2016-03-08 20:56:29 -0600

    Registration, buzz, bad roll detection, idle pong display

--- sketch/CurieDice/CurieDice.ino
+++ sketch/CurieDice/CurieDice.ino
@@ -86,7 +86,6 @@ BLEUnsignedCharCharacteristic diceRollCharacteristic("0x2AC5", BLERead|BLEWrite|
 BLEUnsignedCharCharacteristic diceCommandCharacteristic("0x2A9F", BLERead|BLEWrite|BLENotify);
 
 boolean gConnect = false;
-//int16_t   diceId = 1;
 unsigned char diceId = 1;
 uint16_t  seqNum = 0;
 uint16_t nextSeqNum() {
@@ -94,24 +93,48 @@ uint16_t nextSeqNum() {
   return seqNum;
 }
 bool rollSent = false;
-
+bool registered = false;
 // ************* IMU **************************************************
 int16_t vraw[10]; // raw readings from IMU and info for sending via BLE
                   // [0:5]=AxAyAzGxGyGz, [6]=movement, [7]=stillMs, [8]=diceId, [9]=seqNum
 int vprev[6];     // previous reading
 
+#define MOVE_THRESHOLD  32
+#define GOOD_ROLL       4
 int16_t movement=0;
+int16_t maxMove[6];
+int16_t moves=0;
 unsigned long stillBegin;
 unsigned long stillMillis=0;   // number of sequential zero movements
 
+
+void idleShow()
+{
+  static int imask = 0x1;
+  static int dir = 1;
+  static int prev = 0;
+  if (stillMillis < 5000) return;
+  int p = stillMillis/150;
+  if (p!=prev) {
+    prev = p;
+    if (imask==0x01) dir = 1;
+    if (imask==0x80) dir = 0;
+    if (dir) imask = imask << 1;
+    else imask = imask >> 1;
+    dispLed(0xff,0x000,0);
+    dispLed(imask,0xff0,0);
+  }
+}
 
 int16_t imuRead() {
   for (int i=0; i<6; i++) vprev[i] = vraw[i];
   CurieImu.getMotion6(&vraw[0], &vraw[1], &vraw[2], &vraw[3], &vraw[4], &vraw[5]);
   movement = 0;
+
   for (int i=0; i<6; i++) {
     int m = (abs(vprev[i]-vraw[i])) >> 8;
     if (m>movement) movement = m;
+    if (maxMove[i]==0 && m>MOVE_THRESHOLD) { maxMove[i]=m; moves++;}
   }
   // movement is 8 bits
   int mask = 0x0;
@@ -119,7 +142,7 @@ int16_t imuRead() {
   for (int i=0; i<8; i++) {
     if (movement > (t<<i)) mask = (mask << 1) | 1;
   }
-  if (movement==0) {
+  if (movement==0) { // no movement
     if (stillMillis==0) {
       stillBegin = millis();
       stillMillis = 1;
@@ -128,31 +151,54 @@ int16_t imuRead() {
     else {
       stillMillis = millis() - stillBegin;
       if (stillMillis > 250) {
-        int a = vraw[0]>>8;
-        int b = vraw[1]>>8;
-        int c = vraw[2]>>8;
-        unsigned char roll=0;
-        if (a>0x1d && a<0x3d && b>-10 && b<13 && c>0x12 && c<0x32)
-          {roll=1; dispLed(0x1,0x0f0, 30);}
-        else if (a>0x13 && a<0x37 && b>-13 && b<13 && c>(signed char)0xb3 && c<(signed char)0xd6)
-          {roll=2; dispLed(0x3, 0x0f0, 30);}
-        else if (a>(signed char)0xee && a<10 && b>(signed char)0xb1 && b<(signed char)0xd1 && c>(signed char)0xe6 && c<10)
-          {roll=3; dispLed(0x7, 0x0f0, 30);}
-        else if (a>(signed char)0xed && a<10 && b>0x30 && b<0x50 && c>(signed char)0xeb && c<10)
-          {roll=4; dispLed(0x17, 0x0f0, 30);}
-        else if (a>(signed char)0xc5 && a<(signed char)0xe0 && b>-10 && b<10 && c>0x1a && c<0x36)
-          {roll=5; dispLed(0x37, 0x0f0, 30);}
-        else if (a>(signed char)0xbb && a<(signed char)0xdb && b>-10 && b<10 && c>(signed char)0xbb && c<(signed char)0xdb)
-          {roll=6; dispLed(0x77, 0x0f0, 30);}
-        if (roll!=0 && !rollSent)
-        {
-          diceRollCharacteristic.setValue((diceId << 4) | roll);
-          rollSent = true;
+        if (moves < GOOD_ROLL) {
+          dispLed(0xff, 0x000, 0);
+          dispLed(0xff, 0xf00, 0);
+          moves=0;
+          for (int i=0; i<6; i++) maxMove[i]=0;
+        }
+        else if (!rollSent) {
+          int a = vraw[0]>>8;
+          int b = vraw[1]>>8;
+          int c = vraw[2]>>8;
+          unsigned char roll=0;
+          int disp=0;
+          if (a>0x1d && a<0x3d && b>-10 && b<13 && c>0x12 && c<0x32)
+            {roll=1; dispLed(0x1,0x0f0, 30);}
+          else if (a>0x13 && a<0x37 && b>-13 && b<13 && c>(signed char)0xb3 && c<(signed char)0xd6)
+            {roll=2; disp=0x3;}
+          else if (a>(signed char)0xee && a<10 && b>(signed char)0xb1 && b<(signed char)0xd1 && c>(signed char)0xe6 && c<10)
+            {roll=3; disp=0x7;}
+          else if (a>(signed char)0xed && a<10 && b>0x30 && b<0x50 && c>(signed char)0xeb && c<10)
+            {roll=4; disp=0x17;}
+          else if (a>(signed char)0xc5 && a<(signed char)0xe0 && b>-10 && b<10 && c>0x1a && c<0x36)
+            {roll=5; disp=0x37;}
+          else if (a>(signed char)0xbb && a<(signed char)0xdb && b>-10 && b<10 && c>(signed char)0xbb && c<(signed char)0xdb)
+            {roll=6; disp=0x77;}
+          if (roll!=0) {
+            dispLed(disp, 0x0f0, 50);
+            diceRollCharacteristic.setValue((diceId << 4) | roll);            
+            rollSent = true;
+          }
+          else
+          {
+            moves=0;
+            for (int i=0; i<6; i++) maxMove[i]=0;
+          }
+        } else { // rollSent
+          idleShow();
         }
       }
     }
   }
-  else stillMillis=0;
+  else
+  {
+    if (rollSent==true) {
+      moves=0;
+      for (int i=0; i<6; i++) maxMove[i]=0;
+    }
+    stillMillis=0;
+  }
   
   SERIAL_PRINT(movement);
   SERIAL_PRINT("\t");
@@ -177,11 +223,34 @@ int16_t imuRead() {
   return movement;
 }
 
+unsigned char readDiceId()
+{
+  int a12 = digitalRead(12);
+  int a11 = digitalRead(11);
+  int a10 = digitalRead(10);
+  int a = (a12<<2)|(a11<<1)|a10;
+  switch (a) {
+    case 7: diceId = 0; break;
+    case 3: diceId = 1; break;
+    case 5: diceId = 2; break;
+    case 6:
+    default: diceId = 3; break;
+  }
+  return diceId;
+}
 
 const int ledPin = 13;      // activity LED pin
 boolean blinkState = false; // state of the LED
 // ************* setup **************************************************
 void setup() {
+  // diceId read pins
+  pinMode(12, INPUT_PULLUP);
+  pinMode(11, INPUT_PULLUP);
+  pinMode(10, INPUT_PULLUP);
+  
+  // onboard LED
+  pinMode(ledPin, OUTPUT);
+  
   pinMode(VIBE_PIN, OUTPUT);
   digitalWrite(VIBE_PIN,0);
   
@@ -197,7 +266,7 @@ void setup() {
 
   dispLed(0x01, 0xff0, 0);      // 0 - yellow BLE setup
 
-// *********** BLE **************************************
+// *********** BLE Setup **************************************
   /* Set a local name for the BLE device
      This name will appear in advertising packets
      and can be used by remote devices to identify this BLE device
@@ -210,17 +279,14 @@ void setup() {
   blePeripheral.setEventHandler(BLEConnected, connectHandler);
   blePeripheral.setEventHandler(BLEDisconnected, disconnectHandler);
   diceCommandCharacteristic.setEventHandler(BLEWritten, commandHandler);
-    
-  //diceRollCharacteristic.setValue((unsigned char *)vraw,20);
-
-
   blePeripheral.begin();
   SERIAL_PRINTLN("Bluetooth device active, waiting for connections...");
 
-  dispLed(0x01, 0x0f0, 0);      // 0 - green BLE setup done
+  dispLed(0x01, 0x0f0, 500);      // 0 - green BLE setup done
 
-  dispLed(0x02, 0xff0, 0);      // 1 - yellow IMU setup
+  dispLed(0x02, 0xff0, 500);      // 1 - yellow IMU setup
 
+  // *************** IMU Setup ***************************************
   // initialize device
   SERIAL_PRINTLN("Initializing IMU device...");
   CurieImu.initialize();
@@ -247,56 +313,20 @@ void setup() {
   SERIAL_PRINT(CurieImu.getYGyroOffset()); 
   SERIAL_PRINT("\t"); // 0
   SERIAL_PRINT(CurieImu.getZGyroOffset());
-
-  // To manually configure offset compensation values, 
-  // use the following methods instead of the autoCalibrate...() methods below
-  //    CurieImu.setXGyroOffset(220);
-  //    CurieImu.setYGyroOffset(76);
-  //    CurieImu.setZGyroOffset(-85);
-  //    CurieImu.setXAccelOffset(-76);
-  //    CurieImu.setYAccelOffset(-235);
-  //    CurieImu.setZAccelOffset(168);
-
-  dispLed(0x02, 0x00f, 0);      // 1 - blue IMU - orient dice for calibration
-
-  //SERIAL_PRINTLN("About to calibrate. Make sure your board is stable and upright");
-  //delay(5000);
   
-  //// The board must be resting in a horizontal position for 
-  //// the following calibration procedure to work correctly!
-  //SERIAL_PRINT("Starting Gyroscope calibration...");
-  //CurieImu.autoCalibrateGyroOffset();
-  //SERIAL_PRINTLN(" Done");
-  //SERIAL_PRINT("Starting Acceleration calibration...");
-  //CurieImu.autoCalibrateXAccelOffset(0);
-  //CurieImu.autoCalibrateYAccelOffset(0);
-  //CurieImu.autoCalibrateZAccelOffset(1);
-  //SERIAL_PRINTLN(" Done");
+  dispLed(0x02, 0x00f, 200);      // 1 - blue IMU - orient dice for calibration
 
-  //SERIAL_PRINTLN("Internal sensor offsets AFTER calibration...");
-  //SERIAL_PRINT(CurieImu.getXAccelOffset());
-  //SERIAL_PRINT("\t"); // -76
-  //SERIAL_PRINT(CurieImu.getYAccelOffset());
-  //SERIAL_PRINT("\t"); // -2359
-  //SERIAL_PRINT(CurieImu.getZAccelOffset());
-  //SERIAL_PRINT("\t"); // 1688
-  //SERIAL_PRINT(CurieImu.getXGyroOffset());
-  //SERIAL_PRINT("\t"); // 0
-  //SERIAL_PRINT(CurieImu.getYGyroOffset());
-  //SERIAL_PRINT("\t"); // 0
-  //SERIAL_PRINTLN(CurieImu.getZGyroOffset());
-
-  //SERIAL_PRINTLN("Enabling Gyroscope/Acceleration offset compensation");
   CurieImu.setGyroOffsetEnabled(true);
   CurieImu.setAccelOffsetEnabled(true);
 
-  // configure Arduino LED for activity indicator
-  pinMode(ledPin, OUTPUT);
-
   dispLed(0x02, 0x0f0, 0);      // 1 - green IMU setup done
+  readDiceId();
 
+  for (int i=0; i<6; i++) maxMove[i]=0;
   ledLapse = millis();
 }
+
+
 
 void atten(int16_t *a, int len, int shiftBy)
 {
@@ -322,26 +352,39 @@ void disconnectHandler(BLECentral& central)
   digitalWrite(13, LOW);  
 }
 
+#define COMMAND_REGISTER      1
+#define COMMAND_REGISTER_ACK  2
+#define COMMAND_BUZZ          5
+
 void commandHandler(BLECentral &central, BLECharacteristic &characteristic)
 {
-  int cmd = diceCommandCharacteristic.value();
+  int cvalue = diceCommandCharacteristic.value();
+  switch(cvalue) {
+    case COMMAND_REGISTER_ACK: registered = true; break;
+    case COMMAND_BUZZ:
+      digitalWrite(VIBE_PIN,1);
+      delay(50);
+      digitalWrite(VIBE_PIN,0);
+      delay(50);
+      digitalWrite(VIBE_PIN,1);
+      delay(50);
+      digitalWrite(VIBE_PIN,0);
+      break;
+  }
+}
+
+void registerDice() {
+  diceCommandCharacteristic.setValue((diceId << 4) | COMMAND_REGISTER);
 }
 
 void loop() {
-  blePeripheral.poll();       // current implementation just does delay(1);
+  BLECentral central = blePeripheral.central();
+  if (central && central.connected() && !registered) {
+    registerDice();
+  }
 
   imuRead();
     
-  //if (gConnect) {
-  //  SERIAL_PRINT("Sending: ");
-  //  vraw[6] = (int16_t) movement;
-  //  int16_t stillDur = (stillMillis>32000)? 32000 : stillMillis;
-  //  vraw[7] = stillDur;
-  //  vraw[8] = diceId;
-  //  vraw[9] = nextSeqNum();
-  //  diceRollCharacteristic.setValue((unsigned char*) vraw, 20);
-  //}
-
   delay(50);
   if ((millis()-ledLapse) > 1000) {
     ledLapse = millis();
